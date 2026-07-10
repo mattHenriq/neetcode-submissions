c4cb73f3f89feff950b7959c344f4c8fdeaef3b0
Matheus <46304414+mattHenriq@users.noreply.github.com>
2026-07-10 11:13:48 +0000

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        Map<Character, Integer> contador = new HashMap<>();
+
+        for(char letra : s.toCharArray()){
+            contador.put(letra, contador.getOrDefault(letra, 0) + 1);
+        }
+
+        for(char letra : t.toCharArray()){
+            if(!contador.containsKey(letra)){
+                return false;
+            }
+
+            contador.put(letra, contador.get(letra) - 1);
+
+            if(contador.get(letra) < 0){
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
+}
